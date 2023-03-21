bc51c8c861b9a3291e0d8ba3f937c69179d5bfb6
NicolasNinoViancha <nicolasemilioninoviancha@gmail.com>
2023-03-20 22:03:18 -0500

    👌 IMPROVE: clean code

--- src/features/Contact/styles/index.ts
+++ src/features/Contact/styles/index.ts
@@ -1,9 +1,39 @@
+import { StyleSheet, Dimensions } from 'react-native';
 import { COLORS } from '@app/theme';
-import { StyleSheet } from 'react-native';
+
+const { width } = Dimensions.get('window')
 
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
+        paddingHorizontal: '5%',
         backgroundColor: COLORS.white
+    },
+    message: {
+        fontSize: 20,
+        color: COLORS.black,
+        marginTop: width * 0.04,
+        marginBottom: width * 0.08,
+        textAlign: 'center',
+        fontWeight: 'bold'
+    },
+    buttonSubmit: {
+        width: '100%',
+        backgroundColor: COLORS.black,
+        borderRadius: 4,
+        height: width * 0.1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    textButton: {
+        fontSize: 14,
+        fontWeight: 'bold',
+        color: COLORS.white
+    },
+    ctnInput: {
+        marginBottom: width * 0.04
+    },
+    buttonDisable: {
+        backgroundColor: COLORS.gray
     }
 })
